33be86161422c6663ac8c457828265928ed39b86
Sovelon <solncevv96@mail.ru>
2024-10-11 21:05:56 +0300

    HW done

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,10 +1,43 @@
-#include <assert.h>
 #include "str2int.h"
-
+#include <stdio.h>
+#include <assert.h>
 
 int str2int(const char *str) {
-    if (*str == '-') {
-        assert(1 == 0);
+    int result = 0;
+    int znak = 1;
+    int nummax = 2147483647;
+    int nummin = (-2147483648);
+    
+    while (*str == ' ') {
+        str++;
     }
-    return 0;
+
+    if (*str == '\0') {
+        assert(0); 
+    }
+
+    if (*str == '-') {
+        znak = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+    
+    if (*str == '\0') {
+        assert(0); 
+    }
+
+    while ((*str >= '0' && *str <= '9') && *str != '\0') {
+        if (znak == 1 && result > (nummax - (*str - '0')) / 10) {
+            assert(0);
+        }
+        if (znak == -1 && -result < (nummin + (*str - '0')) / 10) {
+            assert(0);
+        }
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+
+    return result * znak;
 }
+
